5bfad72d3dc16ef7b21721d1f7d6ec8d1c631a9f
Dorielson Martins <dorielson.mv@gmail.com>
2025-08-27 23:30:14 -0300

    Salv entidade assoc para um parte 01

--- src/main/java/com/devsuperior/aula/services/PersonService.java
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -1,4 +1,33 @@
 package com.devsuperior.aula.services;
 
+import com.devsuperior.aula.dto.DepartmentDTO;
+import com.devsuperior.aula.dto.PersonDepartmentDTO;
+import com.devsuperior.aula.entities.Department;
+import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PersonService {
+
+   @Autowired
+   private PersonRepository repository;
+
+    public PersonDepartmentDTO insert (PersonDepartmentDTO dto){
+        Person entity = new Person();
+
+        entity.setName(dto.getName());
+        entity.setSalary(dto.getSalary());
+
+        Department dept = new Department();
+        dept.setId(dto.getDepartment().getId());
+
+        entity.setDepartment(dept);  //associação
+
+        entity = repository.save(entity);
+
+        return new PersonDepartmentDTO(entity);
+    }
+
 }
